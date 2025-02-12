f7b001aa66b6d8183c0b580fe1d7c18deb8630ce
Tristan Robinson <tdr@adatis.co.uk>
2025-02-12 16:08:11 +0000

    Update default gateway in config.py to 'databricks'

--- stroma/config.py
+++ stroma/config.py
@@ -61,7 +61,7 @@ gateways = {}
 # This avoids reading in env variables that may not exist or looking for libraries that are not installed
 
 enabled_gateways = [
-    i.strip().lower() for i in os.getenv("ENABLED_GATEWAYS", "duckdb").split(",")
+    i.strip().lower() for i in os.getenv("ENABLED_GATEWAYS", "databricks").split(",")
 ]
 
 # Make sure default gateway is in enabled
