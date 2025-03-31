9f00dd4a730a8e2465ded2c7357924c7cb45d812
Tristan Robinson <tdr@adatis.co.uk>
2025-03-31 15:49:37 +0100

    Add Azure SQL environment variable configurations to sqlmesh_runner.py

--- sqlmeshrunner/sqlmesh_runner.py
+++ sqlmeshrunner/sqlmesh_runner.py
@@ -4,7 +4,6 @@ import sqlmesh
 import os
 
 # COMMAND ----------
-
 # DBTITLE 1,Get Secrets
 os.environ['DATABRICKS_STATE_DB_HOST'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "ConnectionStringTo-SqlMeshStateDatabase")
 os.environ['DATABRICKS_STATE_DB_USER'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "UsernameTo-SqlMeshStateDatabase")
@@ -13,8 +12,13 @@ os.environ['DATABRICKS_SERVER_HOSTNAME'] = dbutils.secrets.get(scope = "AzureKey
 os.environ['DATABRICKS_HTTP_PATH'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "SqlEndpointTo-DatabricksWorkspace")
 os.environ['DATABRICKS_ACCESS_TOKEN'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "TokenTo-DatabricksAPI")
 
-# COMMAND ----------
+os.environ['AZURE_SQL_SERVER_STATE_HOST'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "ConnectionStringTo-AzureSqlDbStateDatabase")
+os.environ['AZURE_SQL_SERVER_STATE_USER'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "UsernameTo-AzureSqlDbStateDatabase")
+os.environ['AZURE_SQL_SERVER_STATE_PASSWORD'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "PasswordTo-AzureSqlDbStateDatabase")
+os.environ['AZURE_SQL_STATE_DATABASE'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "DatabaseTo-AzureSqlDbStateDatabase")
 
+# COMMAND ----------
+# DBTITLE 1,Set Environment Variables
 os.environ['ENABLED_GATEWAYS']="databricks"
 os.environ['DEFAULT_GATEWAY']='databricks'
 os.environ['STATE_SCHEMA']="stroma"
@@ -25,19 +29,14 @@ os.environ['DATABRICKS_SCHEMA_VOCAB']="lth_omop"
 os.environ['DATABRICKS_CONCURRENT_TASKS']='8'
 os.environ['DATABRICKS_STATE_DB_PORT']='5432'
 os.environ['DATABRICKS_STATE_DB_DATABASE']="sqlmesh_stroma"
-
+os.environ['AZURE_SQL_SERVER_STATE_PORT']='1433'
 
 print('Environment variables set.')
 
 # COMMAND ----------
-
+# DBTITLE 1,Set Context
 # MAGIC %context ../stroma/
 
 # COMMAND ----------
-
-# Uncomment next line to run a migration when SQLMesh upgrade results in a request for migration.
-#  %migrate
-
-# COMMAND ----------
-
-# MAGIC %plan vc --include-unmodified
+# DBTITLE 1,Run Plan
+# MAGIC %plan prod
