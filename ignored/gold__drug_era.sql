7f63d5e6bbd5404ee7ef3bf25484d8c49d7b9745
vvcb <vvcb.n1@gmail.com>
2025-03-31 11:29:41 +0100

    Move era models from ignored into stroma

--- ignored/gold__drug_era.sql
+++
@@ -1,9 +0,0 @@
-MODEL (
-  name gold.drug_era,
-  kind VIEW,
-  cron '@monthly',
-  grain drug_era_id,
-  physical_properties ('delta.tuneFileSizesForRewrites' = FALSE, 'delta.targetFileSize' = '256mb')
-);
-
-@calculate_drug_era(gold)
