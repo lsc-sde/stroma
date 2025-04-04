a76c9f53becbbf7609a7739fb73d1d11cb3390c0
trisdrobinson <150181156+trisdrobinson@users.noreply.github.com>
2025-04-04 08:40:20 +0100

    Update config.py

--- stroma/config.py
+++ stroma/config.py
@@ -100,7 +100,7 @@ if EnumGateway.DATABRICKS in enabled_gateways:
                 access_token=os.environ["DATABRICKS_ACCESS_TOKEN"],
             ),
             state_connection=MSSQLConnectionConfig(  
-                type="azuresql",
+                type="msssql",
                 host=os.environ["AZURE_SQL_SERVER_STATE_HOST"],
                 port=os.environ["AZURE_SQL_SERVER_STATE_PORT"],
                 user=os.environ["AZURE_SQL_SERVER_STATE_USER"],
