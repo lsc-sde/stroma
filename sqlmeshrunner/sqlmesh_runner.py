d2e5394b25d7fa01d6a86a7151a8b7775d4e2014
trisdrobinson <150181156+trisdrobinson@users.noreply.github.com>
2025-04-24 10:07:22 +0100

    Update sqlmesh_runner.py (#35)

--- sqlmeshrunner/sqlmesh_runner.py
+++ sqlmeshrunner/sqlmesh_runner.py
@@ -5,13 +5,9 @@ import os
 
 # COMMAND ----------
 # DBTITLE 1,Get Secrets
-os.environ['DATABRICKS_STATE_DB_HOST'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "ConnectionStringTo-SqlMeshStateDatabase")
-os.environ['DATABRICKS_STATE_DB_USER'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "UsernameTo-SqlMeshStateDatabase")
-os.environ['DATABRICKS_STATE_DB_PASSWORD'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "PasswordTo-SqlMeshStateDatabase")
 os.environ['DATABRICKS_SERVER_HOSTNAME'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "UrlTo-DatabricksWorkspace")
 os.environ['DATABRICKS_HTTP_PATH'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "SqlEndpointTo-DatabricksWorkspace")
 os.environ['DATABRICKS_ACCESS_TOKEN'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "TokenTo-DatabricksAPI")
-
 os.environ['AZURE_SQL_SERVER_STATE_HOST'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "ConnectionStringTo-AzureSqlDbStateDatabase")
 os.environ['AZURE_SQL_SERVER_STATE_USER'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "UsernameTo-AzureSqlDbStateDatabase")
 os.environ['AZURE_SQL_SERVER_STATE_PASSWORD'] = dbutils.secrets.get(scope = "AzureKeyVaultSecrets", key = "PasswordTo-AzureSqlDbStateDatabase")
@@ -27,8 +23,6 @@ os.environ['DATABRICKS_CATALOG_SOURCE']="dbw-lsc-uks-dataprd-dlz-01"
 os.environ['DATABRICKS_SCHEMA_BASE']="lth_omop"
 os.environ['DATABRICKS_SCHEMA_VOCAB']="lth_omop"
 os.environ['DATABRICKS_CONCURRENT_TASKS']='8'
-os.environ['DATABRICKS_STATE_DB_PORT']='5432'
-os.environ['DATABRICKS_STATE_DB_DATABASE']="sqlmesh_stroma"
 os.environ['AZURE_SQL_SERVER_STATE_PORT']='1433'
 
 print('Environment variables set.')
